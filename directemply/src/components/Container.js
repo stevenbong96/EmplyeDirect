2b4228d25eadd3efcca6eeadaa1983646304eb2c
Steven Bong <stevenseal99@yahoo.com>
2020-11-04 19:04:49 -0800

    Added header file to components folder

--- directemply/src/components/Container.js
+++ directemply/src/components/Container.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Header from "./Header";
 
 class Container extends React.Component {
     render(){
         return(
             <div>
                 <Navbar />
+                <Header />
             </div>
         );
     }
